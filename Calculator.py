29ff2d52955b0f1a0882414d91fb0d9e5bc169fb
Harsh Murjani <103986690+hm05@users.noreply.github.com>
2022-11-28 12:28:19 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+'''
+    This program is written by Harsh Murjani.
+    This is a basic calculator which can be used to calculate basic operations like addition, subtraction, multiplication and division.
+    This calculator is made using conditional statement (if... else) in python language.
+    Do check it out https://github.com/hm05 repository link https://github.com/hm05/Calculator
+    This program was written on date 28 November 2022.
+'''
+num1 = int(input("Enter 1st number here: "))
+oper = 
+num2 = int(input("Enter 2nd number here: "))
